93d3c3b1bdaaf1f2ae4941baa5dcdb8fa683c5d6
Thasorn Chalongvorachai <thasorn52111@gmail.com>
2017-08-29 19:49:09 +0700

    Add lab03

---
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/thasorn58070047/simplemydot/MainActivity.java
@@ -0,0 +1,55 @@
+//backup
+package kmitl.lab03.thasorn58070047.simplemydot;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.util.Random;
+import kmitl.lab03.thasorn58070047.simplemydot.model.Dot;
+import kmitl.lab03.thasorn58070047.simplemydot.view.DotView;
+
+public class MainActivity extends AppCompatActivity implements Dot.onDotChangedListener {
+
+    private DotView newDotView;
+    private Dot dot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_dot);
+
+        //Set default Values
+        newDotView = (DotView) findViewById(R.id.newDotView);
+        if (newDotView == null) {
+            Log.d("DEBUG", "NULL!");
+        }
+
+        dot = new Dot(this, 0, 0, 20, 0);
+    }
+
+    public void onRandomDot(View view) {
+        //Random a Dot
+        Random random = new Random();
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        int color = Color.argb(100, r, g, b); //pastel
+        int cenX = random.nextInt(this.newDotView.getWidth());
+        int cenY = random.nextInt(this.newDotView.getHeight());
+        new Dot(this, cenX, cenY, 20, color);
+    }
+
+    @Override
+    public void onDotChanged(Dot dot) {
+        Log.d("DEBUG", newDotView.toString());
+        newDotView.setDot(dot);
+        newDotView.invalidate();
+    }
+
+    public void onDotDelete(View view){
+        newDotView.deleteDot();
+        newDotView.invalidate();
+    }
+}
